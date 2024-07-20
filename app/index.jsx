c09a9c2911cfa63aea24ebf7bbf36e01ff9fe52a
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-21 00:48:14 +0530

    welcome page -onbord screen done

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full items-center justify-center h-full px-4 ">
+        <View className="w-full items-center justify-center min-h-[85vh] px-4 ">
           <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain" />
           <Image source={images.cards} className="max-w-[380-px] w-full  h-[300px]" resizeMode="contain" />
 
@@ -23,7 +23,7 @@ export default function App() {
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
 
-          <CustomButton title="Continue with Email" handlePress={() => {}} containerStyles="w-full mt-7" />
+          <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-7" />
         </View>
       </ScrollView>
 
