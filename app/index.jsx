4211011a5866dadba312b09fdf4414d3eef494cd
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-21 00:43:33 +0530

    button component created

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
@@ -21,8 +22,12 @@ export default function App() {
           </View>
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+
+          <CustomButton title="Continue with Email" handlePress={() => {}} containerStyles="w-full mt-7" />
         </View>
       </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
---
+++ components/CustomButton.jsx
@@ -0,0 +1,12 @@
+import { Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+const CustomButton = ({ title, handlePress, containerStyles, isLoading, textStyles }) => {
+  return (
+    <TouchableOpacity handlePress={handlePress} activeOpacity={0.7} className={`bg-secondary-100 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`} disabled={isLoading}>
+      <Text className={`text-primary text-lg font-psemibold ${textStyles}`}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
