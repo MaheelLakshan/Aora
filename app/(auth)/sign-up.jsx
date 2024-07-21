af98e2c60a46646e7b1dab4d41413f1a59ca7ba4
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-21 16:10:50 +0530

    appwrite configuration and implementation

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,16 +1,32 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
   const [form, setForm] = useState({ username: '', email: '', password: '' });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in the fields');
+    }
+
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -21,7 +37,7 @@ const SignUp = () => {
           <FormField title="Email" value={form.email} handleChangeText={(e) => setForm({ ...form, email: e })} otherStyles="mt-7" keyboardType="email-address" />
           <FormField title="Password" value={form.password} handleChangeText={(e) => setForm({ ...form, password: e })} otherStyles="mt-7" />
 
-          <CustomButton title="Sign up" handlePress={submit} isLoading={isSubmitting} containerStyles="mt-7" />
+          <CustomButton title="Sign Up" handlePress={submit} isLoading={isSubmitting} containerStyles="mt-7" />
           <View className="flex-row justify-center pt-5 gap-2">
             <Text className="text-lg text-gray-100 font-pregular">Already have account?</Text>
             <Link href="/sign-in" className="text-lg font-psemibold text-secondary-100">
