d21f8577c4debe35e08d6adbf1e6716131e36348
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-21 12:13:24 +0530

    sign-in page was deisgned

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -14,6 +19,14 @@ const SignIn = () => {
           <Text className="text-2xl text-white text-semibold font-psemibold mt-10">Log into Aora</Text>
           <FormField title="Email" value={form.email} handleChangeText={(e) => setForm({ ...form, email: e })} otherStyles="mt-7" keyboardType="email-address" />
           <FormField title="Password" value={form.password} handleChangeText={(e) => setForm({ ...form, password: e })} otherStyles="mt-7" />
+
+          <CustomButton title="Sign in" handlePress={submit} isLoading={isSubmitting} containerStyles="mt-7" />
+          <View className="flex-row justify-center pt-5 gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">Don't have account?</Text>
+            <Link href="/sign-up" className="text-lg font-psemibold text-secondary-100">
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
