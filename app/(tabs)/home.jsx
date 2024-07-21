d1001ab15fdc8ef33404e771cf09ff2da126e2ae
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-22 00:36:31 +0530

    create custom hook for fetching

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,26 +1,31 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
+import { getAllPost } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPost);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
     //re call videos -> if any new videos appeared
+    await refetch();
     setRefreshing(false);
   };
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <Text className="text-3xl text-white">{item.id}</Text>}
+        renderItem={({ item }) => <Text className="text-3xl text-white">{item.title}</Text>}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6 ">
             <View className="justify-between items-start flex-row mb-6 ">
